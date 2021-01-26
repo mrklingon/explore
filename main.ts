2fbf54b329cfb35ef54eed9c5bdbe53c51be62d3
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-01-26 16:55:52 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 function doReport () {
     for (let index = 0; index <= Stars.length - 1; index++) {
         basic.showString("" + (Types[index]))
+        if ("M" == Types[index]) {
+            Mfound += 1
+        }
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -14,8 +17,26 @@ function doReport () {
     }
     showStars()
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("M:")
+    basic.showNumber(Mfound)
+    basic.pause(100)
+    showStars()
+})
 input.onButtonPressed(Button.AB, function () {
     buildStars()
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.Target)
+    }
     showStars()
 })
 input.onButtonPressed(Button.B, function () {
@@ -53,7 +74,23 @@ let sy = 0
 let sx = 0
 let Types: string[] = []
 let Stars: number[] = []
+let Mfound = 0
 let TypeList: string[] = []
+images.createBigImage(`
+    . . . . . . . . . .
+    # # # # . # # # # .
+    . . # . . . # . . .
+    . . . # # # # . . .
+    . . . . . . . . . .
+    `).scrollImage(1, 200)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(1, 200)
 TypeList = ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 buildStars()
 showStars()
+Mfound = 0
