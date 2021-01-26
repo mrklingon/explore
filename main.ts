7bac989f0896f02cc02c2dedabf41239d58f29d6
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-01-26 15:45:44 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+function doReport () {
+    for (let index = 0; index <= Stars.length - 1; index++) {
+        basic.showString("" + (Types[index]))
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        findCoord(Stars[index])
+        led.plotBrightness(sx, sy, Magnitude[index])
+        basic.pause(500)
+    }
+    showStars()
+}
+input.onButtonPressed(Button.AB, function () {
+    buildStars()
+    showStars()
+})
+input.onButtonPressed(Button.B, function () {
+    doReport()
+})
 function buildStars () {
     Types = []
     Stars = []
@@ -5,7 +28,7 @@ function buildStars () {
     for (let index = 0; index < randint(5, 13); index++) {
         Stars.push(randint(0, 24))
         Magnitude.push(randint(50, 242))
-        Types.push(TypeList[randint(0, TypeList.length)])
+        Types.push(TypeList[randint(0, TypeList.length - 1)])
     }
 }
 function findCoord (num: number) {
@@ -13,20 +36,24 @@ function findCoord (num: number) {
     sy = num % 5
 }
 function showStars () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     for (let index = 0; index <= Stars.length - 1; index++) {
         findCoord(Stars[index])
         led.plotBrightness(sx, sy, Magnitude[index])
     }
 }
+let Magnitude: number[] = []
 let sy = 0
 let sx = 0
-let Magnitude: number[] = []
-let Stars: number[] = []
 let Types: string[] = []
+let Stars: number[] = []
 let TypeList: string[] = []
 TypeList = ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 buildStars()
 showStars()
-basic.forever(function () {
-	
-})
